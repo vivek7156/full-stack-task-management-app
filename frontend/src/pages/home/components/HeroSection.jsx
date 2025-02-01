52206924d103f1d1e9351608b0f3e40907dff27d
Vivek Singh <142485319+vivek7156@users.noreply.github.com>
2025-02-01 09:05:22 +0530

    implemented spline

--- frontend/src/pages/home/components/HeroSection.jsx
+++ frontend/src/pages/home/components/HeroSection.jsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Spline from '@splinetool/react-spline';
 
 const HeroSection = () => {
   return (
-        <div className="relative h-[60vh] md:h-[80vh] bg-gradient-to-r from-orange-400 to-orange-600">
-        <div className="absolute inset-0 bg-black/10" />
-        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white px-4 text-center">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4 leaping-tight">Welcome to YumRush</h1>
+    <div className="relative h-[70vh] md:h-[80vh] bg-gradient-to-r from-orange-400 to-orange-600">
+      <div className="absolute inset-0 bg-black/10" />
+      <div className="relative z-10 flex flex-col md:flex-row items-center justify-center h-full text-white px-4 text-center">
+        <div className="md:w-1/2">
+          <h1 className="text-3xl md:text-6xl font-bold mb-4 leaping-tight">Welcome to YumRush</h1>
           <p className="text-xl md:text-xl mb-8 max-w-2xl">Delicious food delivered to your doorstep</p>
           <Link 
             to="/menu" 
@@ -14,8 +16,12 @@ const HeroSection = () => {
             Order Now
           </Link>
         </div>
+        <div className="md:w-1/2 mt-8 md:mt-0 rounded-md overflow-hidden shadow-lg h-[40vh] md:h-[50vh] w-full">
+          <Spline scene="https://prod.spline.design/VwT-XxCosiHfONxx/scene.splinecode" style={{ pointerEvents: 'none' }} />
+        </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
